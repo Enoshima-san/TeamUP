d2fad9de6e881878982f51d303c24e498fdc2db9
Ilyas Fardaoui <ilyasfardaoui@gmail.com>
2026-03-13 15:10:15 +0000

    AI: improve Python file src\teamup\infra\repositories\__init__.py

--- src/teamup/infra/repositories/__init__.py
+++ src/teamup/infra/repositories/__init__.py
@@ -1,3 +1,7 @@
+```python
+# Import the UserRepository class from the user_repository module
 from .user_repository import UserRepository
 
+# Specify the classes and functions that should be imported when using the current module
 __all__ = ["UserRepository"]
+```
