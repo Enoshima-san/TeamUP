af00797fec8f081d91456e160c2dd929e67ab72b
Ilyas Fardaoui <ilyasfardaoui@gmail.com>
2026-03-13 15:08:40 +0000

    AI: improve Python file src\teamup\application\interfaces\__init__.py

--- src/teamup/application/interfaces/__init__.py
+++ src/teamup/application/interfaces/__init__.py
@@ -1,5 +1,9 @@
+```python
+# Import the interface for the authentication service
 from .auth_service import IAuthService
 
+# Define the list of public symbols for this module
 __all__ = [
     "IAuthService",
 ]
+```
