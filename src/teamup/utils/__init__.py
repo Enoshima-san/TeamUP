0424aaae23fd1b8167c2e317eff55708857cba00
Ilyas Fardaoui <ilyasfardaoui@gmail.com>
2026-03-13 15:10:54 +0000

    AI: improve Python file src\teamup\utils\__init__.py

--- src/teamup/utils/__init__.py
+++ src/teamup/utils/__init__.py
@@ -1,3 +1,9 @@
 from .db_create_all import run_create_all
 
+"""
+Module containing database creation functionality.
+
+This module exports a single function, `run_create_all`, which can be used to create all database tables.
+"""
+
 __all__ = ["run_create_all"]
---
+++ src/teamup/utils/__init__.py.bak.20260313151047
@@ -0,0 +1,3 @@
+from .db_create_all import run_create_all
+
+__all__ = ["run_create_all"]
